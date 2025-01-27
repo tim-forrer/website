eaa98a52e35a9d140259ee1831f3f7982299a03b
tim@mbp <timforrer16@gmail.com>
2025-01-27 11:13:49 +0900

    change default date string

--- components/Date.js
+++ components/Date.js
@@ -1,7 +1,7 @@
 import { parseISO, format} from "date-fns";
 
 // Parses `dateString` according to the format given
-export default function Date({ dateString, dateFormat = 'do MMM yyyy' }) {
+export default function Date({ dateString, dateFormat = 'MMM yyyy' }) {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, dateFormat)}</time>;
 }
