35851daa748df829990fef633bd1404f905fae51
tim@mbp <timforrer16@gmail.com>
2025-01-28 15:55:04 +0900

    add hashtag

--- components/TagGroup.js
+++ components/TagGroup.js
@@ -1,9 +1,11 @@
 import { Group, Badge } from "@mantine/core";
 import styles from "./TagGroup.module.css";
+import { HiHashtag } from "react-icons/hi2";
 
 function TagsAsBadges(data, size) {
+  const hashtag = <HiHashtag size={10} />
   return data.tags.map((tag) => (
-    <Badge size={size} key={data.id} className={styles.badge}>{tag}</Badge>
+    <Badge size={size} key={data.id} className={styles.badge} leftSection={hashtag}>{tag}</Badge>
   ));
 }
 
