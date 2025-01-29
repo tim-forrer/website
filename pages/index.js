22d9da2f01abb05c4cfcebc4621b7afa3571b328
tim@mbp <timforrer16@gmail.com>
2025-01-29 12:36:43 +0900

    finish homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { Stack, Title, Text, Badge, Group, SemiCircleProgress } from '@mantine/core';
 import ChatUI from '../components/ChatUI';
+import Link from 'next/link';
+import { person } from '../public/me'
 
 export async function getStaticProps() {
   return {
@@ -9,7 +12,35 @@ export async function getStaticProps() {
 }
 
 export default function Home() {
+  const kaggleLink = person.socials.find(social => social.name === "Kaggle")?.link;
   return (
-    <ChatUI />
+    <Stack gap={30}>
+      <Text size='xl'>
+        I <Link href="/projects">prod at the edges of quantum theory</Link> and see if what comes back can be used for building better quantum computers.<br/>
+        <br />
+        In my spare time, I  <Link href={kaggleLink} target="_blank" rel="noopener noreferrer">teach myself data science</Link>, <Link href="#ll-tim">build LLMs</Link>, and try to take nice photos.
+      </Text>
+      <Stack gap={8}>
+        <Title order={1}>Skills</Title>
+        <Group justify='space-between' align='center'>
+          {person.languages.map((language) => (
+            <SemiCircleProgress
+            size={150}
+            key={language.label} 
+            label={language.label}
+            value={language.value}
+            emptySegmentColor="var(--mantine-color-gray-3)"
+          />))}
+        </Group>
+        <Group gap='xs'>
+          {person.skills.map((skill) => (<Badge size='lg'>{skill}</Badge>))}
+        </Group>
+      </Stack>
+      <Stack gap={8}>
+        <Title order={1} id="ll-tim">LL-tiM - a personalised LLM</Title>
+        <ChatUI />
+        {/* You can read about the process of building LL-tiM here! */}
+      </Stack>
+    </Stack>
   );
 }
