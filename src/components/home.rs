3bebb35dc75ef0dce50b6c40f6d387f427d7a068
tim@mbp <timforrer16@gmail.com>
2025-07-29 13:22:46 +0900

    :sparkles: Add markdown

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,5 +1,11 @@
 use crate::Route;
 use dioxus::prelude::*;
+use dioxus_markdown::Markdown;
+
+const MARKDOWN_SRC: &str = r#"
+## Math
+- $e^{i\pi} + 1 = 0$
+"#;
 
 #[component]
 pub fn Home() -> Element {
@@ -7,6 +13,7 @@ pub fn Home() -> Element {
         div {
             id: "home",
             h1 {"Tim Forrer"}
+            Markdown { src: MARKDOWN_SRC }
             p {"Physics doctoral student at the University of Tokyo."}
             br {  }
             p {"Outside of research, I'm interested in:"}
